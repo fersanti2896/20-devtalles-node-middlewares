35d7976b83d360c7df3c5d89879e0ca32cf830b2
Fernando Nicolás <fersanti2896@gmail.com>
2024-01-24 01:28:03 -0600

    Creando modelos de producto y categoria.

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,3 @@
 
-export { UserModel } from './mongodb/models';
+export { CategoryModel, ProductModel, UserModel } from './mongodb/models';
 export { MongoDatabase } from './mongodb/mongo-database';
